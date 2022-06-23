dbfc1509122024d2d1e4fee21af48568f6827931
Hugo Ruscitti <hugoruscitti@gmail.com>
2022-06-23 20:08:09 -0300

    Creando prueba de personajes y que se pueda cerrar con el teclado.

---
+++ prueba-rotar-escenario/close-with-scape.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	set_process(true)
+
+func _input(event):
+	if event is InputEventKey and event.is_pressed():
+		if(event.scancode == KEY_ESCAPE):
+			get_tree().quit()
