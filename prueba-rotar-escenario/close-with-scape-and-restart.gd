581beeb6568f5c74ea4b50c22387c9781eaf520a
Hugo Ruscitti <hugoruscitti@gmail.com>
2022-06-23 21:09:55 -0300

    Haciendo que el personaje pueda bajar solo.

--- prueba-rotar-escenario/close-with-scape-and-restart.gd
+++ prueba-rotar-escenario/close-with-scape-and-restart.gd
@@ -7,3 +7,6 @@ func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		if(event.scancode == KEY_ESCAPE):
 			get_tree().quit()
+		if(event.scancode == KEY_R):
+			get_tree().reload_current_scene()
+
